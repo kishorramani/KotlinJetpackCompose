ee7babed0057d844e1c8f81df9a555f22769667e
kishor ramani <kishorkeepsmiling@gmail.com>
2021-04-20 03:48:41 +0530

    day5 - TextStyle

--- app/src/main/java/com/kishorramani/kotlinjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/kishorramani/kotlinjetpackcompose/MainActivity.kt
@@ -3,36 +3,84 @@ package com.kishorramani.kotlinjetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
-import com.kishorramani.kotlinjetpackcompose.ui.theme.KotlinJetpackComposeTheme
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            KotlinJetpackComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            StylingText()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun StylingText() {
+    val fontFamily = FontFamily(
+        Font(R.font.josefinsans_bold, FontWeight.Bold),
+        Font(R.font.josefinsans_light, FontWeight.Light),
+        Font(R.font.josefinsans_medium, FontWeight.Medium),
+        Font(R.font.josefinsans_regular, FontWeight.Normal),
+        Font(R.font.josefinsans_semibold, FontWeight.SemiBold),
+        Font(R.font.josefinsans_thin, FontWeight.Thin),
+    )
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFF101010))
+    ) {
+        Text(
+            text = buildAnnotatedString {
+                withStyle(
+                    style = SpanStyle(
+                        color = Color.Green,
+                        fontSize = 80.sp
+                    )
+                ) {
+                    append("J")
+                }
+                append("etpack ")
+                withStyle(
+                    style = SpanStyle(
+                        color = Color.Green,
+                        fontSize = 80.sp
+                    )
+                ) {
+                    append("C")
+                }
+                append("ompose")
+            },
+            color = Color.White,
+            fontSize = 30.sp,
+            fontFamily = fontFamily,
+            fontWeight = FontWeight.Bold,
+            fontStyle = FontStyle.Italic,
+            textAlign = TextAlign.Center,
+            textDecoration = TextDecoration.Underline
+        )
+    }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun DefaultPreview() {
-    KotlinJetpackComposeTheme {
-        Greeting("Android")
-    }
+fun PreviewStylingText() {
+    StylingText()
 }
