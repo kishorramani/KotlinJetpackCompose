21d81616bc0d902f1d5b934ff4892ce4034b5b67
kishor ramani <kishorkeepsmiling@gmail.com>
2021-04-21 03:31:37 +0530

    day8 - Lists

--- app/src/main/java/com/kishorramani/kotlinjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/kishorramani/kotlinjetpackcompose/MainActivity.kt
@@ -3,36 +3,71 @@ package com.kishorramani.kotlinjetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import com.kishorramani.kotlinjetpackcompose.ui.theme.KotlinJetpackComposeTheme
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            KotlinJetpackComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+            /*val scrollState = rememberScrollState()
+            Column(
+                modifier = Modifier.verticalScroll(scrollState)
+            ) {
+                for (i in 1..50) {
+                    Text(
+                        text = "item $i",
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.SemiBold,
+                        textAlign = TextAlign.Start,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(10.dp)
+                    )
+                }
+            }*/
+
+            LazyColumn {
+                items(500) {
+                    Text(
+                        text = "item $it",
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.SemiBold,
+                        textAlign = TextAlign.Start,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(10.dp)
+                    )
                 }
             }
+
+            /*LazyColumn {
+                itemsIndexed(
+                    listOf("Kajal", "kishor", "Kalpesh", "Kailash", "Keval", "Kinjal", "Kanubhai")
+                ) {
+                    index, item ->
+                    Text(
+                        text = "$index - $item",
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.SemiBold,
+                        textAlign = TextAlign.Start,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(10.dp)
+                    )
+                }
+            }*/
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    KotlinJetpackComposeTheme {
-        Greeting("Android")
     }
 }
